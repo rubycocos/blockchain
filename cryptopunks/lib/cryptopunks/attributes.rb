4c1c4c1a92ba4e3223a349aae8d49f43dbdbe7ae
Gerald Bauer <gerald.bauer@gmail.com>
2021-02-13 21:01:25 +0100

    up

--- cryptopunks/lib/cryptopunks/attributes.rb
+++ cryptopunks/lib/cryptopunks/attributes.rb
@@ -27,68 +27,67 @@ TYPES = [
 ## note: does NOT include 5 punk types (that is, alien, ape, zombie, female, male)
 
 
-ATTRIBUTES = [
- { name: 'Mouth',  values: ['Cigarette',
-                            'Pipe',
-                            'Vape',
-                            'Medical Mask',
-                           ]
+ACCESSORY_TYPES = [
+ { name: 'Mouth',  accessories: [ { name: 'Cigarette',    limit: 961 },
+                                  { name: 'Pipe',         limit: 317 },
+                                  { name: 'Vape',         limit: 272 },
+                                  { name: 'Medical Mask', limit: 175 }]
  },
- { name: 'Nose',   values: ['Clown Nose']
+ { name: 'Nose',   accessories: [ { name: 'Clown Nose', limit: 212 }]
  },
  {
    name: 'Hair',
-   values: ['Wild Blonde',
-            'Wild Hair',
-            'Dark Hair',
-            'Stringy Hair',
-            'Crazy Hair',
-            'Messy Hair',
-            'Mohawk',
-            'Mohawk Thin',
-            'Mohawk Dark',
-            'Peak Spike',
-            'Frumpy Hair',
-            'Clown Hair Green',
-            'Shaved Head',
-            'Vampire Hair',
-            'Red Mohawk',
-            'Blonde Bob',
-            'Straight Hair Dark',
-            'Straight Hair',
-            'Purple Hair',
-            'Straight Hair Blonde',
-            'Wild White Hair',
-            'Half Shaved',
-            'Pigtails',
-            'Orange Side',
-            'Do-rag',
-            'Tiara',
-            'Blonde Short',
-            'Pink With Hat',
-            'Beanie',
-            'Headband',
-            'Bandana',
-            'Hoodie',
-            'Top Hat',
-            'Tassle Hat',
-            'Cap',
-            'Knitted Cap',
-            'Cap Forward',
-            'Police Cap',
-            'Fedora',
-            'Pilot Helmet',
-            'Cowboy Hat']
+   accessories: [ { name: 'Wild Blonde',      limit: 144 },
+                  { name: 'Wild Hair',        limit: 447 },
+                  { name: 'Dark Hair',        limit: 157 },
+                  { name: 'Stringy Hair',     limit: 463 },
+                  { name: 'Crazy Hair',       limit: 414 },
+                  { name: 'Messy Hair',       limit: 460 },
+                  { name: 'Mohawk',           limit: 441 },
+                  { name: 'Mohawk Thin',      limit: 441 },
+                  { name: 'Mohawk Dark',      limit: 429 },
+                  { name: 'Peak Spike',       limit: 303 },
+                  { name: 'Frumpy Hair',      limit: 442 },
+                  { name: 'Clown Hair Green', limit: 148 },
+                  { name: 'Shaved Head',      limit: 300 },
+                  { name: 'Vampire Hair',     limit: 147 },
+                  { name: 'Red Mohawk',       limit: 147 },
+                  { name: 'Blonde Bob',       limit: 147 },
+                  { name: 'Straight Hair Dark', limit: 148 },
+                  { name: 'Straight Hair',    limit: 151 },
+                  { name: 'Purple Hair',      limit: 165 },
+                  { name: 'Straight Hair Blonde', limit: 144 },
+                  { name: 'Wild White Hair',  limit: 136 },
+                  { name: 'Half Shaved',      limit: 147 },
+                  { name: 'Pigtails',         limit: 94 },
+                  { name: 'Orange Side',      limit: 68 },
+                  { name: 'Do-rag',           limit: 300 },
+                  { name: 'Tiara',            limit: 55 },
+                  { name: 'Blonde Short',     limit: 129 },
+                  { name: 'Pink With Hat',    limit: 95 },
+                  { name: 'Beanie',           limit: 44 },
+                  { name: 'Headband', limit: 406 },
+                  { name: 'Bandana', limit: 481 },
+                  { name: 'Hoodie', limit: 259 },
+                  { name: 'Top Hat', limit: 115 },
+                  { name: 'Tassle Hat', limit: 178 },
+                  { name: 'Cap', limit: 351 },
+                  { name: 'Knitted Cap', limit: 419 },
+                  { name: 'Cap Forward', limit: 254 },
+                  { name: 'Police Cap', limit: 203 },
+                  { name: 'Fedora', limit: 186 },
+                  { name: 'Pilot Helmet', limit: 54 },
+                  { name: 'Cowboy Hat', limit: 142 }]
   },
   {
     name: 'Beard',
-    values: ['Normal Beard',
-             'Normal Beard Black',
-             'Front Beard Dark',
-             'Front Beard',
-             'Shadow Beard',
-             'Luxurious Beard',
-             'Big Beard',
+    accessories: [{ name: 'Normal Beard', limit: 292 },
+                  { name: 'Normal Beard Black', limit: 289 },
+                  { name: 'Front Beard Dark', limit: 260 },
+                  { name: 'Front Beard', limit: 273 },
+                  { name: 'Shadow Beard', limit: 526 },
+                  { name: 'Luxurious Beard', limit: 286 },
+                  { name: 'Big Beard', limit: 146 },
              'Chinstrap',
              'Mustache',
              'Muttonchops',
@@ -97,16 +96,16 @@ ATTRIBUTES = [
   },
   {
     name: 'Ears',
-    values: ['Earring']
+    accessories: [{ name: 'Earring', limit: 2459 }]
   },
   {
     name: 'Eyes',
-    values: ['Blue Eye Shadow',
-             'Purple Eye Shadow',
-             'Green Eye Shadow',
-             'Welding Goggles',
-             'VR',
-             '3D Glasses',
+    accessories: [{ name: 'Blue Eye Shadow', limit: 266 },
+                  { name: 'Purple Eye Shadow', limit: 262 },
+                  { name: 'Green Eye Shadow', limit: 271 },
+                  { name: 'Welding Goggles', limit: 86 },
+                  { name: 'VR', limit: 332 },
+                  { name: '3D Glasses', limit: 286 },
              'Clown Eyes Blue',
              'Clown Eyes Green',
              'Small Shades',
@@ -120,26 +119,27 @@ ATTRIBUTES = [
   },
   {
     name: 'Lips',
-    values: ['Purple Lipstick',
-             'Black Lipstick',
-             'Hot Lipstick']
+    accessories: [{ name: 'Purple Lipstick', limit: 655 },
+                  { name: 'Black Lipstick', limit: 617 },
+                  { name: 'Hot Lipstick', limit: 696 } ]
   },
   {
     name: 'Face',
-    values: ['Spots', 'Mole']
+    accessories: [{ name: 'Spots', limit: 124 },
+                  { name: 'Mole',  limit: 644 }]
   },
   {
     name: 'Neck',
-    values: ['Choker',
-             'Silver Chain',
-             'Gold Chain']
+    accessories: [{ name: 'Choker', limit: 48 },
+                  { name: 'Silver Chain', limit: 156 },
+                  { name: 'Gold Chain', limit: 169 }]
   },
-  { name: 'Cheeks', values: ['Rosy Cheeks']
+  { name: 'Cheeks', accessories: [{ name: 'Rosy Cheeks', limit: 128 }]
   },
-  { name: 'Teeth',  values: ['Buck Teeth']
+  { name: 'Teeth',  accessories: [{ name: 'Buck Teeth', limit: 78 }]
   },
-  { name: 'Emotion', values: ['Frown',
-                              'Smile']
+  { name: 'Emotion', accessories: [{ name: 'Frown', limit: 261 },
+                                   { name: 'Smile', limit: 238 }]
   }
 ]
 
